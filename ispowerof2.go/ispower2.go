ef371f8af235b0e95bb32ad0a536a5d2afa24279
moonyango <mosesadrian825@gmail.com>
2024-06-06 15:33:24 +0300

    more functions

--- ispowerof2.go/ispower2.go
+++ ispowerof2.go/ispower2.go
@@ -12,8 +12,43 @@ func main() {
 	fmt.Println(isPowerof2(66))
 	fmt.Println(isPowerof2(64))
 	fmt.Println(isPowerof2(127))
+	fmt.Println("active bits of 7:", activeBits(7))
+	fmt.Println("active bits of 8:", activeBits(8))
+	fmt.Println("trailing zeroes of 6:", trailingzeros(6))
+	fmt.Println("trailing zeroes of 8:", trailingzeros(8))
+	fmt.Printf("Reversed bits of 1:%08b\n", reverseBits(0b00001111))
+	fmt.Println(0b11100110)
 }
 
 func isPowerof2(n int) bool {
 	return (n&(n-1) == 0) && n > 0
+}
+func activeBits(n int) int {
+	count := 0
+	for n > 0 {
+		count += n & 1
+		n >>= 1
+	}
+	return count
+}
+
+func trailingzeros(n int) int {
+	if n  == 0 {
+		return 32
+	}
+	count := 0
+	for (n & 1) == 0 {
+		count++
+		n >>= 1
+	}
+	return count
+}
+
+func reverseBits(n uint8) uint8 {
+	var result uint8
+	for i :=0; i< 8 ;i ++ {
+		result = (result << 1 ) | (n & 1)
+		n >>= 1
+	}
+	return result
 }
--- reversebits/main.go
+++ reversebits/main.go
@@ -8,17 +8,40 @@ func main() {
 
 	fmt.Println(ReverseBits(0b10001000))
 	fmt.Println(ReverseBits(0b10000000))
-	fmt.Println(ReverseBits(0b1000000))
-	fmt.Println(ReverseBits(0b10000000))
+	fmt.Println(ReverseBits(0b1010000))
+	fmt.Println(ReverseBits(0b11000000))
+	fmt.Println(R(0b11000000))
+	fmt.Printf("%b\n", Swapbit(0b10010111))
+	fmt.Println(PrintBits(6))
 }
 
 func ReverseBits(oct byte) byte {
 
 	var result byte
 	for i := 0; i < 8; i++ {
-		result <<= 1
+		result = result << 1
 		result |= oct & 1
 		oct >>= 1
 	}
 	return result
+}
+
+func Swapbit(oct byte) byte {
+	return oct>>4 | oct<<4
+}
+
+func PrintBits(n int) string {
+	s := ""
+	for n > 0 {
+		if n%2 == 1 {
+			s = "1" + s
+		} else {
+			s = "0" + s
+		}
+		n /= 2
+	}
+	return s
+}
+func R(o byte) byte {
+	return (o >> 7 & 1) | (o>>6&1)<<1 | (o>>5&1)<<2 | (o>>4&1)<<3 | (o>>3&1)<<4 | (o>>2&1)<<5 | (o>>1&1)<<6 | (o << 7)
 }
