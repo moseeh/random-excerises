c9647a14a82b1a5d9dd3465efdc67e6084243bb5
moonyango <mosesadrian825@gmail.com>
2024-08-22 14:25:51 +0300

    grouping

--- grouping/grouping.go
+++ grouping/grouping.go
@@ -56,3 +56,76 @@ func main() {
 		counter++
 	}
 }
+
+func split(s string) []string {
+	res := []string{}
+
+	tmp := ""
+	for _, char := range s {
+		if char == ' ' {
+			if tmp != "" {
+				res = append(res, tmp)
+				tmp = ""
+			}
+		} else {
+			tmp += string(char)
+		}
+	}
+	if tmp != "" {
+		res = append(res, tmp)
+	}
+	return res
+
+}
+func containsPipe(s string) bool {
+	for _, char := range s {
+		if char == '|' {
+			return true
+		}
+	}
+	return false
+}
+func getmatch(a string) []string {
+	tmp := ""
+	res := []string{}
+	for _, char := range a {
+		if char == ')' || char == '(' {
+			continue
+		}
+		if char == '|' {
+			res = append(res, tmp)
+			tmp = ""
+		} else {
+			tmp += string(char)
+		}
+	}
+	if tmp != "" {
+		res = append(res, tmp)
+	}
+	return res
+}
+
+// his hi
+func contains(a, b string) bool {
+	size := len(b)
+	for i := 0; i < len(a); i++ {
+		size := i + size
+		if size >= len(a) {
+			size = len(a)
+		}
+		if b == a[i:size] {
+			return true
+		}
+
+	}
+	return false
+}
+
+func isempty(s string) bool {
+	for _, c := range s {
+		if c != ' ' {
+			return false
+		}
+	}
+	return true
+}
